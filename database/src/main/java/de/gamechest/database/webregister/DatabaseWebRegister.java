caefff38d7e231293406b1dd1d9f197a9c5b8551
ByteList <niklas.bytelist@gmail.com>
2018-04-15 15:56:58 +0200

    Update web-register database

--- database/src/main/java/de/gamechest/database/webregister/DatabaseWebRegister.java
+++ database/src/main/java/de/gamechest/database/webregister/DatabaseWebRegister.java
@@ -1,11 +1,15 @@
 package de.gamechest.database.webregister;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.Block;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.model.Filters;
 import de.gamechest.database.DatabaseCollection;
+import de.gamechest.database.DatabaseElement;
 import de.gamechest.database.DatabaseManager;
 import org.bson.Document;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 /**
@@ -35,5 +39,32 @@ public class DatabaseWebRegister {
                 .append(DatabaseWebRegisterObject.STATE.getName(), "0");
 
         databaseManager.getCollection(databaseCollection).insertOne(document);
+    }
+
+    public ArrayList<Document> getAllByState(String state) {
+        FindIterable<Document> find = databaseManager.getCollection(databaseCollection)
+                .find(Filters.eq(DatabaseWebRegisterObject.STATE.getName(), state));
+
+        ArrayList<Document> documents = new ArrayList<>();
+        find.forEach((Block<? super Document>) documents::add);
+
+        return documents;
+    }
+
+    public void setDatabaseObject(UUID uuid, DatabaseWebRegisterObject databaseWebRegisterObject, Object value) {
+        BasicDBObject doc = new BasicDBObject();
+        doc.append("$set", new BasicDBObject().append(databaseWebRegisterObject.getName(), value));
+
+        BasicDBObject basicDBObject = new BasicDBObject().append(DatabaseWebRegisterObject.UUID.getName(), uuid.toString());
+        databaseManager.getCollection(databaseCollection).updateOne(basicDBObject, doc);
+    }
+
+    public DatabaseElement getDatabaseElement(UUID uuid, DatabaseWebRegisterObject databaseWebRegisterObject) {
+        FindIterable<Document> find = databaseManager.getCollection(databaseCollection).find(Filters.eq(DatabaseWebRegisterObject.UUID.getName(), uuid));
+        Document document = find.first();
+
+        if (document == null) return null;
+
+        return new DatabaseElement(document.get(databaseWebRegisterObject.getName()));
     }
 }
