15751771f45233f259c8d9c7c8d18797f2480abb
ByteList <n.emmrich@wrba.ch>
2018-10-11 11:21:12 +0200

    test

--- verify/src/main/java/de/gamechest/verify/bot/TeamspeakBot.java
+++ verify/src/main/java/de/gamechest/verify/bot/TeamspeakBot.java
@@ -177,15 +177,15 @@ public class TeamspeakBot {
                 }
             } else {
                 if(!channel.getName().equals("Support | Warteraum [Geschlossen]")) {
-                    properties.put(ChannelProperty.CHANNEL_MAXCLIENTS, "0");
-                    properties.put(ChannelProperty.CHANNEL_FLAG_MAXCLIENTS_UNLIMITED, "0");
-                    properties.put(ChannelProperty.CHANNEL_NAME, "Support | Warteraum [Geschlossen]");
-                    properties.put(ChannelProperty.CHANNEL_DESCRIPTION, noSupportMessage+channel.getDescription());
                     this.getApi().editChannel(this.supportWaitChannelId, properties);
                     clientInChannel.forEach(client -> {
                         this.getApiAsync().kickClientFromChannel(client);
                         this.getApiAsync().sendPrivateMessage(client.getId(), "Der Support wurde geschlossen, da kein Teammitglied zum Support zur Verfügung steht.");
                     });
+                    properties.put(ChannelProperty.CHANNEL_MAXCLIENTS, "0");
+                    properties.put(ChannelProperty.CHANNEL_FLAG_MAXCLIENTS_UNLIMITED, "0");
+                    properties.put(ChannelProperty.CHANNEL_NAME, "Support | Warteraum [Geschlossen]");
+                    properties.put(ChannelProperty.CHANNEL_DESCRIPTION, noSupportMessage+channel.getDescription());
                 }
             }
         });
