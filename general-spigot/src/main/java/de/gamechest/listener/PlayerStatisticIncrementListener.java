ba17bd9a183ba5ed03b6b2ba200fc6cccc58df54
ByteList <niklas.bytelist@gmail.com>
2019-03-02 02:19:32 +0100

    test

--- general-spigot/src/main/java/de/gamechest/listener/PlayerStatisticIncrementListener.java
+++ general-spigot/src/main/java/de/gamechest/listener/PlayerStatisticIncrementListener.java
@@ -1,10 +1,12 @@
 package de.gamechest.listener;
 
 import com.google.common.base.CaseFormat;
-import de.gamechest.common.AsyncTasks;
+import com.mongodb.MongoClient;
 import de.gamechest.GameChest;
+import de.gamechest.common.AsyncTasks;
 import de.gamechest.database.stats.network.DatabaseNetworkStatsObject;
-import org.bson.Document;
+import org.bson.BsonDocument;
+import org.bson.BsonInt64;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -53,15 +55,16 @@ public class PlayerStatisticIncrementListener implements Listener {
         AsyncTasks.getInstance().runTaskAsync(()-> {
             gameChest.getDatabaseManager().getDatabaseNetworkStats().createPlayer(player.getUniqueId());
 
-            Document document = gameChest.getDatabaseManager().getDatabaseNetworkStats().
-                    getDatabaseElement(player.getUniqueId(), DatabaseNetworkStatsObject.MINECRAFT).getAsDocument();
+            BsonDocument document = gameChest.getDatabaseManager().getDatabaseNetworkStats().
+                    getDatabaseElement(player.getUniqueId(), DatabaseNetworkStatsObject.MINECRAFT).getAsDocument()
+                    .toBsonDocument(BsonDocument.class, MongoClient.getDefaultCodecRegistry());
 
             int value = 0;
             if(document.containsKey(finalStatistic)) {
-                value = document.getInteger(finalStatistic);
+                value = document.getInt64(finalStatistic).intValue();
             }
 
-            document.append(finalStatistic, value+add);
+            document.append(finalStatistic, new BsonInt64(value+add));
 
             gameChest.getDatabaseManager().getDatabaseNetworkStats().setDatabaseObject(player.getUniqueId(), DatabaseNetworkStatsObject.MINECRAFT, document);
         });
